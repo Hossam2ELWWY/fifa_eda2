21d11c066aecd7950e7e172be467051c1b32ab9f
Hossam2ELWWY <149169850+Hossam2ELWWY@users.noreply.github.com>
2024-02-09 14:32:40 +0200

    Update players.py

--- pages/players.py
+++ pages/players.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np 
 import plotly.express as px 
 import streamlit  as st 
-df = pd.read_csv(r"C:\Users\Maydoum\Downloads\Compressed\Project 12_FIFA EDA\fifa_eda.csv")
+df = pd.read_csv(r"fifa_eda.csv")
 df.dropna(inplace=True)
 st.title('Players')
 sum = df["Name"].count()
