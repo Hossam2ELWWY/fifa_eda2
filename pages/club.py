dd3fe0ef9e84559b57aff8318f9ef1ac3fc42726
Hossam2ELWWY <149169850+Hossam2ELWWY@users.noreply.github.com>
2024-02-09 14:27:08 +0200

    Add files via upload

---
+++ pages/club.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import numpy as np 
+import plotly.express as px 
+import streamlit  as st 
+df = pd.read_csv(r"C:\Users\Maydoum\Downloads\Compressed\Project 12_FIFA EDA\fifa_eda.csv")
+df.dropna(inplace=True)
+st.title('Club')
+sum = df["Name"].count()
+st.write("sum of club ", sum)
+club_count = df.groupby("Club")['Name'].count().reset_index()
+vis_club_count = px.bar(club_count, x="Club", y="Name", title="Number of Players per Club")
+
+st.plotly_chart(vis_club_count)
+st.dataframe(club_count)
